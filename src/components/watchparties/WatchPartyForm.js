0b09be9d24059c9fabad684dd40e5f35cb879d05
Matthew Machurek <mrjm2005@yahoo.com>
2021-03-19 15:23:41 -0500

    form is showing up

--- src/components/watchparties/WatchPartyForm.js
+++ src/components/watchparties/WatchPartyForm.js
@@ -1,64 +1,104 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useEffect, useRef } from "react"
 import { useHistory } from "react-router-dom"
 import { WatchPartyContext } from "./WatchPartyProvider.js"
+import { GameContext } from "../games/GameProvider.js"
 
 
 export const WatchPartyForm = () => {
-    const history = useHistory()
-    const { createWatchParty, getWatchParties } = useContext(WatchPartyContext)
-    const [watchparty, setWatchParty] = useState({})
+    // const history = useHistory()
+    const { watchparty, createWatchParty } = useContext(WatchPartyContext)
+    const { games, getGames } = useContext(GameContext)
+    // const [watchparty, setWatchParty] = useState({})
+
+    const name = useRef(null)
+    const time = useRef(null)
+    const location = useRef(null)
+    const fans = useRef(null)
 
     useEffect(() => {
-        getWatchParties()
-            .then(getGames)
+        getGames()
     }, [])
 
-    // const changeEventState = (domEvent) => {
-    //     // ...
+    // const handleInputChange = (event) => {
+    //     const newWatchParty = Object.assign({}, watchparty)
+    //     watchParty[event.target.name] = event.target.value
+    //     setWatchParty(newWatchParty)
+    //     // console.log("watchparty", watchparty)
     // }
+
+    const history = useHistory()
+
+    const game = games.find(g => g.id === history.gameId) || {}
 
     const createNewWatchParty = () => {
         createWatchParty({
             user_id: parseInt(localStorage.getItem("Token")),
             name: name.current.value,
-            scheduled_time: scheduled_time.current.value,
+            scheduled_time: time.current.value,
             game: game.name,
             location: location.current.value,
-            number_of_fans: number_of_fans.current.value,
+            number_of_fans: fans.current.value,
         })
-            .then((watchparty) => {
-                props.history.push(`/watchparties`)
-
+            .then(() => {
+                history.push(`/watchparties`)
             })
+        // console.log("push", props)
     }
+
+    return (
+        <form className="watchpartyForm">
+            <h2 className="watchpartyForm__title">Create Watch Party</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" id="name" ref={name}
+                        required autoFocus className="form-control" placeholder="Name"
+                        proptype="varchar"
+                    // defaultValue={watchparty.name}
+                    // onChange={handleInputChange}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="time">Scheduled Time: </label>
+                    <input type="text" id="time" ref={time}
+                        required autoFocus className="form-control" placeholder="Time"
+                        proptype="varchar"
+                    // defaultValue={watchparty.scheduled_time}
+                    // onChange={handleInputChange}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="location">Location: </label>
+                    <input type="text" id="location" ref={location}
+                        required autoFocus className="form-control" placeholder="Location"
+                        proptype="varchar"
+                    // defaultValue={watchparty.location}
+                    // onChange={handleInputChange}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="fans">Max Fans Allowed: </label>
+                    <input type="text" id="fans" ref={fans}
+                        required autoFocus className="form-control" placeholder="Max Fans"
+                        proptype="varchar"
+                    // defaultValue={watchparty.number_of_fans}
+                    // onChange={handleInputChange}
+                    />
+                </div>
+            </fieldset>
+
+            <button type="create"
+                onClick={evt => {
+                    evt.preventDefault() // Prevent browser from submitting the form
+                    createNewWatchParty()
+                }}
+            ></button>
+        </form >
+    )
 }
-
-return (
-    <form className="watchpartyForm">
-        <h2 className="watchpartyForm__title">Create Watch Party</h2>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="gameId">Game: </label>
-                <select name="gameId" className="form-control"
-                    value={currentEvent.gameId}
-                    onChange={changeEventState}>
-                    <option value="0">Select a game...</option>
-                    {
-                        games.map(game => (
-                            <option></option>
-                        ))
-                    }
-                </select>
-            </div>
-        </fieldset>
-
-        {/* Create the rest of the input fields */}
-
-        <button type="create"
-            onClick={evt => {
-                evt.preventDefault() // Prevent browser from submitting the form
-                createNewWatchParty()
-            }}
-        ></button>
-    </form >
-)
